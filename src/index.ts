6081c77d853853cd52a6802d596c7839b02c6ad8
Raphaël Thériault <raphael_theriault@outlook.com>
2020-12-01 19:52:44 -0500

    Don't use AoC scores

--- src/index.ts
+++ src/index.ts
@@ -22,34 +22,27 @@ async function listener(message: Message, config: Config) {
     }
 
     const lb = await leaderboard(config);
-    const members = Object.values(lb.members).sort(
-        (a, b) => (b.local_score - a.local_score) * 100 + (b.stars - a.stars)
-    );
+    const members = Object.values(lb.members).sort((a, b) => b.stars - a.stars);
 
     const tableData = [];
     let lastRank = -1;
-    let lastScore = -1;
-    for (const [i, { name, local_score, stars }] of members.entries()) {
-        const rank = local_score === lastScore ? lastRank : i + 1;
-        tableData.push([rank, name, local_score, stars]);
+    let lastStars = -1;
+    for (const [i, { name, stars }] of members.entries()) {
+        const rank = stars === lastStars ? lastRank : i + 1;
+        tableData.push([rank, name, stars]);
 
         lastRank = rank;
-        lastScore = local_score;
+        lastStars = stars;
     }
-    const tableContents = table(
-        [["Rank", "Name", "Score", "Stars"], ...tableData],
-        {
-            columns: {
-                0: { alignment: "right" },
-                1: { alignment: "left" },
-                2: { alignment: "right" },
-                3: { alignment: "right" },
-            },
-            drawHorizontalLine: (i, length) =>
-                i === 0 || i === 1 || i === length,
-            border: getBorderCharacters("norc"),
-        }
-    );
+    const tableContents = table([["Rank", "Name", "Stars"], ...tableData], {
+        columns: {
+            0: { alignment: "right" },
+            1: { alignment: "left" },
+            2: { alignment: "right" },
+        },
+        drawHorizontalLine: (i, length) => i === 0 || i === 1 || i === length,
+        border: getBorderCharacters("norc"),
+    });
 
     await message.channel.send("```\n" + tableContents + "\n```");
 }
